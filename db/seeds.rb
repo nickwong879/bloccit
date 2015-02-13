c118d99717a965820676fb1643af6a9554233f67
Nick Wong <nickwong879@gmail.com>
2015-02-14 09:45:05 +1100

    Seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+# Create posts
+50.times do
+	Post.create!(
+		title:  Faker::Lorem.sentence,
+		body:   Faker::Lorem.paragraph
+		)
+end
+posts = Post.all
+
+# Create comments
+100.times do
+	Comment.create!(
+		post:  posts.sample,
+		body:  Faker::Lorem.paragraph
+		)
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
