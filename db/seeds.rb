fd8bffe9cff433d03e2445d0043f40851ad02030
nickwong879@gmail.com <nickwong879@gmail.com>
2015-02-24 21:46:38 +1100

    associations checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,46 @@
 
 require 'faker'
 
+# Create users
+5.times do
+	user = User.new(
+		name:  Faker::Name.name,
+		email: Faker::Internet.email,
+		password:  Faker::Lorem.characters(10)
+		)
+	user.skip_confirmation!
+	user.save!
+end
+users = User.all
+
+
 # Create posts
 50.times do
 	Post.create!(
+		user:   users.sample,
 		title:  Faker::Lorem.sentence,
 		body:   Faker::Lorem.paragraph
 		)
 end
-
-Post.create!(
-		title:  "This is a unique post",
-		body:  "This is a unique body content for my post"
-		)
-
 posts = Post.all
 
 # Create comments
 100.times do
 	Comment.create!(
+		# user: users.sample, # we have not yet associated Users with comments
 		post:  posts.sample,
 		body:  Faker::Lorem.paragraph
 		)
 end
 
-Comment.create!(
-		post:  post.last,
-		body:  "This is a unique comment for my post"
-		)
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+	email: 'nickwong879@gmail.com',
+	password:  'helloworld'
+	)
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
