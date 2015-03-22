038d98bdd49487bf213a543ddc081521c97df469
nickwong879@gmail.com <nickwong879@gmail.com>
2015-03-22 21:41:20 +1100

    Updated completion of voting checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,13 +40,16 @@ summaries = Summary.all
 
 # Create posts
 50.times do
-	Post.create!(
+	post = Post.create!(
 		user:   users.sample,
 		topic:  topics.sample,
-#		summary: summaries.sample,
 		title:  Faker::Lorem.sentence,
 		body:   Faker::Lorem.paragraph
 		)
+		# set the created_at to a time within the last year
+		post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+		post.update_rank
+
 end
 posts = Post.all
 
