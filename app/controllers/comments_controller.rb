9e54cb766a231c1ed4c127241e3ebb2b8f2bb4e5
nickwong879 <nickwong879@gmail.com>
2015-04-03 09:47:15 +1100

    Ajax creating checkpoint and assignment completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,25 @@ class CommentsController <ApplicationController
 
 def create
 
-  @post = Post.find(params[:post_id])
-    @comment = current_user.comments.build(params.require(:comment).permit(:body))
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
+
+    @comment = current_user.comments.build( comment_params )
+    @comment.post = @post
+    @new_comment = Comment.new
+    
     authorize @comment
-    @comment.user = current_user
 
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again"
-      redirect_to [@post.topic, @post]
     end
-	
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
 end
 
 def destroy
@@ -34,6 +39,12 @@ def destroy
     format.js
   end
 
+end
+
+private
+
+def comment_params
+  params.require(:comment).permit(:body)
 end
 
 end
